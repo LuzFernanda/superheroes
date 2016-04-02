9acb2f6ee012fb078731823f09a85734fd6e8dbd
Luz Arriaga <luz.arriaga.h95@gmail.com>
2016-04-01 21:07:00 -0600

    Se agrego batman vs superman

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   get 'pages/batmanvssuperman'
 
+  root 'pages#batmanvssuperman'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
